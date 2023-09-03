3142eeaf40e5309c9176303b4820b2ca8d0a92f1
Owen Andrew Jørgensen <owen@jorgensenconsult.no>
2023-09-03 09:47:00 +0200

    feat: mapping to Kernel32.dll

--- HDF5-CSharp/Hdf5Common.cs
+++ HDF5-CSharp/Hdf5Common.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using HDF5CSharp.DataTypes;
 using System.Text.RegularExpressions;
+using HDF5CSharp.Helpers;
 
 namespace HDF5CSharp
 {
@@ -50,28 +51,32 @@ namespace HDF5CSharp
             return arr;
         }
 
-        [GeneratedRegex("[æøåöäï€]+", RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.CultureInvariant)]
-        private static partial Regex _generateValidatorRegex();
-
-        private static Regex _illegalCharacterValidator = _generateValidatorRegex();
-
         /// <summary>
         /// Opens a Hdf-5 file
         /// </summary>
         /// <param name="filename">Filename</param>
         /// <param name="readOnly">Wether or not to open readonly</param>
+        /// <param name="attemptShortPath">Whether or not to attempt to correct path to windows 8.3 short path form (ASCII-safe)</param>
         /// <returns>File Id</returns>
         /// <exception cref="ArgumentNullException">When filename is null or whitespace</exception>
         /// <exception cref="ArgumentOutOfRangeException">When filename contains illegal characters.</exception>
-        public static long OpenFile(string filename, bool readOnly = false)
+        /// <exception cref="InvalidOperationException">When attemptShortPath is set to true on a non-windows machine</exception>
+        public static long OpenFile(string filename, bool readOnly = false, bool attemptShortPath = false)
         {
             if (string.IsNullOrWhiteSpace(filename))
             {
                 throw new ArgumentNullException(nameof(filename), "Argument cannot be null, empty or whitespace.");
             }
-            if (_illegalCharacterValidator.IsMatch(filename))
+            if (attemptShortPath)
             {
-                throw new ArgumentOutOfRangeException(nameof(filename), "Argument contains illegal characters. HDF5.PInvoke cannot handle file paths with non-ascii characters.");
+                filename = filename.ToShortPath();
+            }
+            else
+            {
+                if (_illegalCharacterValidator.IsMatch(filename))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(filename), "Argument contains illegal characters. HDF5.PInvoke cannot handle file paths with non-ascii characters.");
+                }
             }
             uint access = (readOnly) ? H5F.ACC_RDONLY : H5F.ACC_RDWR;
             var fileId = H5F.open(filename, access);
@@ -79,21 +84,35 @@ namespace HDF5CSharp
         }
 
         /// <summary>
+        /// Regex that matches characters deemed invalid in filenames by HFD.PInvoke library.
+        /// </summary>
+        private static readonly Regex _illegalCharacterValidator = new("[æøåöäï€]+", RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.CultureInvariant);
+
+        /// <summary>
         /// Creates a Hdf-5 file
         /// </summary>
         /// <param name="filename">Filename</param>
+        /// <param name="attemptShortPath">Whether or not to attempt to correct path to windows 8.3 short path form (ASCII-safe)</param>
         /// <returns>File Id</returns>
         /// <exception cref="ArgumentNullException">When filename is null or whitespace</exception>
         /// <exception cref="ArgumentOutOfRangeException">When filename contains illegal characters.</exception>
-        public static long CreateFile(string filename)
+        /// <exception cref="InvalidOperationException">When attemptShortPath is set to true on a non-windows machine</exception>
+        public static long CreateFile(string filename, bool attemptShortPath = false)
         {
             if (string.IsNullOrWhiteSpace(filename))
             {
                 throw new ArgumentNullException(nameof(filename), "Argument cannot be null, empty or whitespace.");
             }
-            if (_illegalCharacterValidator.IsMatch(filename))
+            if (attemptShortPath)
             {
-                throw new ArgumentOutOfRangeException(nameof(filename), "Argument contains illegal characters. HDF5.PInvoke cannot handle file paths with non-ascii characters.");
+                filename = filename.ToShortPath();
+            }
+            else
+            {
+                if (_illegalCharacterValidator.IsMatch(filename))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(filename), "Argument contains illegal characters. HDF5.PInvoke cannot handle file paths with non-ascii characters.");
+                }
             }
             return H5F.create(filename, H5F.ACC_TRUNC);
         }
---
+++ HDF5-CSharp/Helpers/FilePathHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDF5CSharp.Helpers;
+
+/// <summary>
+/// File path helper method(s)
+/// </summary>
+internal static class FilePathHelper
+{
+    /// <summary>
+    /// Converts a long path with possible non-ascii characters to an ascii safe path in the 8.3 safe format.
+    /// </summary>
+    /// <param name="longPath"></param>
+    /// <returns>An 8.3 format path</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    internal static string ToShortPath(this string longPath)
+    {
+        if(Environment.OSVersion.Platform != PlatformID.Win32NT)
+        {
+            throw new InvalidOperationException("The extension method ToShortPath(this string longPath) cannot be called in a non-windows operating system context.");
+        }
+        StringBuilder sb = new(255);
+        _ = GetShortPathName(longPath, sb, sb.Capacity);
+        return sb.ToString();
+    }
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, EntryPoint = "GetShortPAthNameW", SetLastError = true)]
+    internal extern static uint GetShortPathName([MarshalAs(UnmanagedType.LPTStr)] string longpath, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder shortPath, int bufferSize);
+}
